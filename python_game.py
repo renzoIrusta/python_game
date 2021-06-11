d5d4c8af8ef66c6d08570ccdb0511019615819f4
renzoIrusta <renzoirusta22@gmail.com>
2021-06-11 17:57:15 -0300

    matriz creada

--- python_game.py
+++ python_game.py
@@ -1,3 +1,5 @@
+import random
+
 # Lista de letras
 letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
@@ -9,6 +11,26 @@ def imprimir_matriz(matriz):
             print(f"{c:3}", end="")
         print()
 
+# Función crear coordenadas
+
+def coordenadas(matriz):
+    tam = len( matriz )
+    for i in range(tam):
+        matriz[0][i] = i
+        matriz[i][0] = i
+
+
+
 # Creación de matriz
+# 
+n = int(input("Ingrese el tamaño de la matriz: "))
 
+matriz = [[random.choice(letras) for col in range(n + 1)] for row in range(n + 1)]
 
+imprimir_matriz(matriz)
+
+print()
+
+coordenadas(matriz)
+
+imprimir_matriz(matriz)

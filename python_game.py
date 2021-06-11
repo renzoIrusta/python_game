042de81938e0c53e4e8b2f041dcc3110f74bebf7
renzoIrusta <renzoirusta22@gmail.com>
2021-06-11 16:20:56 -0300

    creado

---
+++ python_game.py
@@ -0,0 +1,14 @@
+# Lista de letras
+letras = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, Ñ, O, P, Q, R, S, T, U, V, W, X, Y, Z]
+
+# Función imprimir matriz
+
+def imprimir_matriz(matriz):
+    for f in matriz:
+        for c in f:
+            print(f"{c:3}", end="")
+        print()
+
+# Creación de matriz
+
+

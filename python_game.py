49f703abf2f8e797cc96408e719de8e6434987dc
renzoIrusta <renzoirusta22@gmail.com>
2021-06-12 19:30:26 -0300

    actualizado

--- python_game.py
+++ python_game.py
@@ -2,6 +2,7 @@ import random
 
 # Lista de letras
 letras = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+palabras = ["hola", "casa", "chau", "tateti"]
 
 # Función imprimir matriz
 
@@ -19,10 +20,24 @@ def coordenadas(matriz):
         matriz[0][i] = i
         matriz[i][0] = i
 
+# Función para ordenar palabras dentro de la matriz
+
+def orden_random(matriz, palabras):
+    tam = len( matriz )
+    orden = ["horiz", "vert"]
+    for palabra in palabras:
+        elecc = random.choice(orden)
+        coor = random.randint(1, tam - 1)
+        if elecc == "horiz": 
+            for i in range( len(palabra) ):
+                matriz[coor][i + 1] = palabra[i]
+        if elecc == "vert":
+            for i in range( len(palabra) ):
+                matriz[i + 1][coor] = palabra[i]
 
 
 # Creación de matriz
-# 
+
 n = int(input("Ingrese el tamaño de la matriz: "))
 
 matriz = [[random.choice(letras) for col in range(n + 1)] for row in range(n + 1)]
@@ -34,3 +49,11 @@ print()
 coordenadas(matriz)
 
 imprimir_matriz(matriz)
+
+print()
+
+orden_random(matriz, palabras)
+
+imprimir_matriz(matriz)
+
+print(matriz[0][1])
